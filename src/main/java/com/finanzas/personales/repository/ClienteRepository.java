b2fabd7ce80d63a7df7ec5ceb5577cda12dccc0e
Carlos Mario Tuebrquia <carlosmario40@gmail.com>
2026-06-01 21:58:49 -0500

    Add method to search Clientes by multiple fields

--- src/main/java/com/finanzas/personales/repository/ClienteRepository.java
+++ src/main/java/com/finanzas/personales/repository/ClienteRepository.java
@@ -3,7 +3,9 @@ package com.finanzas.personales.repository;
 import com.finanzas.personales.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByNombreContainingIgnoreCaseOrApellidoContainingIgnoreCaseOrDocumentoContainingIgnoreCase(String nombre, String apellido, String documento);
 }
